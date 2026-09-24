d83342dbebafc07ee060a663fb650a3795c5d0e0
Thomas Falcon <Thomas.falcon@crackerbarrel.com>
2026-09-24 12:41:33 -0600

    Route nested profile APIs on Vercel

---
+++ api/v1/profiles/[...path].py
@@ -0,0 +1,5 @@
+"""Vercel catch-all entrypoint for nested profile-management routes."""
+
+from api.main import app
+
+__all__ = ["app"]
---
+++ api/v1/resume-versions/[...path].py
@@ -0,0 +1,5 @@
+"""Vercel catch-all entrypoint for nested resume-version routes."""
+
+from api.main import app
+
+__all__ = ["app"]
